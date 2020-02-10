7eea595186f96fa41061a68afa64bb6128e3e713
karim fereidoni <kf.worknet@gmail.com>
2020-02-10 10:00:01 +0330

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const VueSSRServerPlugin = require("vue-server-renderer/server-plugin");
 const VueSSRClientPlugin = require("vue-server-renderer/client-plugin");
 const nodeExternals = require("webpack-node-externals");
 const merge = require("lodash.merge");
+const path = require("path");
 
 const TARGET_NODE = process.env.WEBPACK_TARGET === "node";
 const target = TARGET_NODE ? "server" : "client";
